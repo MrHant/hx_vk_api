bd3bb6a705c10eebe339f85648b073c65cc525ee
Loutchansky Oleg <oleg@loutchansky.com>
2015-09-06 16:19:12 +0300

    Add Enviroment cheker

--- vk/Vk.hx
+++ vk/Vk.hx
@@ -5,9 +5,25 @@ import vk.api.Connection;
  * @author Artyom Silivonchik
  */
 class Vk extends Connection
-{
+{	
+	var isWorkEnviroment : Bool = false;
+	
 	public function new(flashVars: Dynamic) 
 	{
 		super(flashVars);
+		
+		if (flashVars.api_id == null)
+		{
+			isWorkEnviroment = false;
+		}
+		else
+		{
+			isWorkEnviroment = true;
+		}
+	}
+	
+	public function isVKEnvironment():Bool
+	{
+		return isWorkEnviroment;
 	}
 }
